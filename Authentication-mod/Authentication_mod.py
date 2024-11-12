9cab8885f749ae416ef4bda42b5adf16d09c6236
Alfie Tompsett <alfietompsett@outlook.com>
2024-11-12 19:40:38 +0000

    Added the start of using SQL and encryption

--- Authentication-mod/Authentication_mod.py
+++ Authentication-mod/Authentication_mod.py
@@ -1,11 +1,13 @@
+import sqlite3, bcrypt
 
+"""connection = pyodbc.connect('Driver={SQL Server};'
+                      'Server=server_name;'
+                      'Database=database_name;'
+                      'Trusted_Connection=yes;')"""
 
-
-
-
-
-
-
+def usercheck(user):
+    pass
+    
 
 
 
