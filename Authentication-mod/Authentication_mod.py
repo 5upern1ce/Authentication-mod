bfdfbdf2dfa429f748256823a9311df701f9030f
Alfie Tompsett <alfietompsett@outlook.com>
2024-11-12 20:11:12 +0000

    Start mapping out functions

--- Authentication-mod/Authentication_mod.py
+++ Authentication-mod/Authentication_mod.py
@@ -1,19 +1,32 @@
 import sqlite3, bcrypt
 
 """connection = pyodbc.connect('Driver={SQL Server};'
-                      'Server=server_name;'
-                      'Database=database_name;'
+                      'Server=localhost;'
+                      'Database=logins;'
                       'Trusted_Connection=yes;')"""
 
-def usercheck(user):
+#Inputs the username 
+def UserIn():
+    user = input("What is your username? \n")
+    return user
+
+#Stores User in an SQL server
+def StoreUser(user):
     pass
-    
 
+#Inputs the password
+def PasswordIn():
+    passwd = input("What is your password? \n")
+    return passwd
 
+#Stores the Password in an SQL server
+def storepass(passwd):
+    pass
 
 
 def main():
-    pass
+    print(userin())
+    print(passwordin())
 
 
 if __name__ == "__main__":
